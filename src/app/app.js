a67fad4e7c13f883b11b37b8c3eafa393f7e4228
katban <grancen@wp.pl>
2015-11-18 16:53:28 +0100

    TIC-41: show ticket list (basic version)

--- src/app/app.js
+++ src/app/app.js
@@ -4,4 +4,19 @@
 (function () {
     var app = angular.module('tickets', []);
 
+    app.controller('TicketList', function ($scope, $http) {
+
+        var url = 'http://localhost:8080/tickets-filter/app_dev.php/';
+
+        var getTickets = function () {
+            $http.get(url)
+                .then(function (response) {
+                    console.log(response);
+                    $scope.ticketList = response.data;
+                })
+        };
+        getTickets();
+
+    });
+
 })();
