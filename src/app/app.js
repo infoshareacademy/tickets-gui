0fcd299f3507f9c0d9be0db752b23c9162274536
katban <grancen@wp.pl>
2015-11-20 09:11:23 +0100

    TIC-41: basic data clearing

--- src/app/app.js
+++ src/app/app.js
@@ -5,16 +5,41 @@
     var app = angular.module('tickets', []);
 
     app.controller('TicketList', function ($scope, $http) {
+        var getTickets = function () {
+            //
+            var tickietClear = [];
+            $http.get('null/data.json')
+            .then(function (response) {
+                angular.forEach(response.data, function(ticket) {
+                    ticket.description = cleanString(ticket.description);
+                    tickietClear.push(ticket);
+                });
+                $scope.ticketList = tickietClear;
+            })
+        };
 
         var url = 'http://localhost:8080/tickets-filter/app_dev.php/';
-        //
-        var getTickets = function () {
-            $http.get('null/data.json')
-                .then(function (response) {
-                    console.log(response);
-                    $scope.ticketList = response.data;
-                })
+
+
+        var cleanString = function (string) {
+            var temp = string;
+            while (1) {
+                if(temp.indexOf('<') >= 0 && temp.indexOf('>') >= 0 && temp.indexOf('>')>temp.indexOf('<')) {
+                    temp = temp.replace(temp.substring(temp.indexOf('<'),temp.indexOf('>')+1), '');
+                } else {
+                    break;
+                }
+            }
+            return temp;
         };
+
+        $http.get('null/data.json').then(function (response) {
+            angular.forEach(response.data, function(ticket) {
+                ticket.description = cleanString(ticket.description);
+                $scope.ticketList = ticket;
+            });
+        })
+
         getTickets();
 
 
