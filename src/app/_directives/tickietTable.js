33b1e1f0495d7cb856fafebe8ed86fecb8ca419c
katban <grancen@wp.pl>
2015-11-23 15:31:18 +0100

    TIC-41: refactor code (moved printng list to directive)

---
+++ src/app/_directives/tickietTable.js
@@ -0,0 +1,58 @@
+/**
+ * Created by katban on 23.11.15.
+ */
+angular.module("tickets")
+    .directive('tickietTable', function() {
+        return {
+            restrict: 'E',
+            templateUrl: '_directives/tickiet-table.html',
+            transclude: true,
+            scope: {},
+            controller: function ($scope, $http) {
+
+                $http.get('null/data.json')
+                    .then(function (response) {
+                        var tickietClear = [];
+                        angular.forEach(response.data, function(ticket) {
+                            ticket.description = cleanString(ticket.description);
+                            tickietClear.push(ticket);
+                        });
+                        $scope.tickietsList = tickietClear;
+
+                    });
+
+                $scope.reorder = function (field) {
+                    $scope.desc = $scope.sortingField === field ? !$scope.desc : false;
+                    $scope.sortingField = field;
+                };
+
+                var cleanString = function (string) {
+                    var temp = string;
+                    while (1) {
+                        // remove comment's tags
+                        if(temp.indexOf('<!--') >= 0 && temp.indexOf('-->') >= 0 && temp.indexOf('-->')>temp.indexOf('<!--')) {
+                            temp = temp.replace(temp.substring(temp.indexOf('<!--'),temp.indexOf('-->')+3), '');
+                        }
+                        // remove all html tags
+                        else if (temp.indexOf('<') >= 0 && temp.indexOf('>') >= 0 && temp.indexOf('>')>temp.indexOf('<')) {
+                            temp = temp.replace(temp.substring(temp.indexOf('<'),temp.indexOf('>')+1), '');
+                        }
+                        // remove declaration of css
+                        else if (temp.indexOf('{') >= 0 && temp.indexOf('}') >= 0 && temp.indexOf('}')>temp.indexOf('{')) {
+                            temp = temp.replace(temp.substring(temp.indexOf('{'),temp.indexOf('}')+1), '');
+                        }
+                        //remove character's code
+                        else if ((temp.indexOf('&') >= 0 && temp.indexOf(';') >= 0 && temp.indexOf(';')>temp.indexOf('&'))) {
+                            temp = temp.replace(temp.substring(temp.indexOf('&'),temp.indexOf(';')+1), ' ');
+                        }
+                        //escape from while
+                        else  {
+                            break;
+                        }
+                    }
+                    return temp.trim();
+                };
+
+            }
+        }
+    });
--- src/app/app.js
+++ src/app/app.js
@@ -6,49 +6,5 @@
 
     app.controller('TickietsController', function ($scope, $http) {
 
-        //http://localhost:8080/tickets-filter/app_dev.php/
-        $http.get('null/data.json')
-            .then(function (response) {
-                var tickietClear = [];
-                angular.forEach(response.data, function(ticket) {
-                    ticket.description = cleanString(ticket.description);
-                    tickietClear.push(ticket);
-                });
-                $scope.tickietsList = tickietClear;
-
-            });
-
-        $scope.reorder = function (field) {
-            $scope.desc = $scope.sortingField === field ? !$scope.desc : false;
-            $scope.sortingField = field;
-        };
-
-        var cleanString = function (string) {
-            var temp = string;
-            while (1) {
-                // remove comment's tags
-                if(temp.indexOf('<!--') >= 0 && temp.indexOf('-->') >= 0 && temp.indexOf('-->')>temp.indexOf('<!--')) {
-                    temp = temp.replace(temp.substring(temp.indexOf('<!--'),temp.indexOf('-->')+3), '');
-                }
-                // remove all html tags
-                else if (temp.indexOf('<') >= 0 && temp.indexOf('>') >= 0 && temp.indexOf('>')>temp.indexOf('<')) {
-                    temp = temp.replace(temp.substring(temp.indexOf('<'),temp.indexOf('>')+1), '');
-                }
-                // remove declaration of css
-                else if (temp.indexOf('{') >= 0 && temp.indexOf('}') >= 0 && temp.indexOf('}')>temp.indexOf('{')) {
-                    temp = temp.replace(temp.substring(temp.indexOf('{'),temp.indexOf('}')+1), '');
-                }
-                //remove character's code
-                else if ((temp.indexOf('&') >= 0 && temp.indexOf(';') >= 0 && temp.indexOf(';')>temp.indexOf('&'))) {
-                    temp = temp.replace(temp.substring(temp.indexOf('&'),temp.indexOf(';')+1), ' ');
-                }
-                //escape from while
-                else  {
-                    break;
-                }
-            }
-            return temp.trim();
-        };
-
     })
 })();
