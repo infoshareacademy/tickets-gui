e45a93219ccbae438a50362818eab6225592c7e3
krasai <krasuskip@gmail.com>
2015-11-25 16:19:50 +0100

    TIC-42  Synchronization button added

--- src/app/controllers/dashboard.js
+++ src/app/controllers/dashboard.js
@@ -8,8 +8,18 @@
  * Controller of tickets
  */
 angular.module('tickets')
-  .controller('DashboardCtrl', function($scope, $state) {
+  .controller('DashboardCtrl', function($scope, $state, $http) {
 
     $scope.$state = $state;
+    $scope.sync = 'Database synchronization started. \nPlease be patient :)';
+
+      $scope.synchronizeDb = function() {
+        $http.get('http://test.tickets-processor.infoshareaca.nazwa.pl/app_dev.php/update')
+            .success(function(data){
+            })
+            .error(function(err){
+              $scope.sync = 'Database synchronization failed. \nWe are sorry :)';
+            })
+      }
 
   });
