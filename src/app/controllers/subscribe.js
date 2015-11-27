4c733d248ecbf6f4f87c25101da24e38ff955211
paoolskoolsky <pawel.skolski@gmail.com>
2015-11-27 14:47:01 +0100

    the end

--- src/app/controllers/subscribe.js
+++ src/app/controllers/subscribe.js
@@ -2,19 +2,36 @@
  * Created by paoolskoolsky on 27.11.15.
  */
 angular.module('tickets')
-    .controller('SubscribeCtrl', function($scope, $state, $http) {
+    .controller('SubscribeCtrl', function ($scope, $state, $http) {
 
-        $scope.subscribeSuccess = false;
+        $scope.subscribeMessage = '';
 
-   $scope.submit = function (email, name) {
+        $scope.submit = function (email, name) {
 
 
-       $http.post('http://', {
-           'email' : email,
-           'name' : name
-       })
-           .then(function () {
-               $scope.subscribeSuccess = true;
-           })
-   }
+            $http.post('http://localhost:3000/subscribers', {
+                'email': email,
+                'name': name
+            })
+                .success(function () {
+                    $scope.subscribeMessage = 'Success - You add your email to newsletter list';
+                })
+                .error(function () {
+                    $scope.subscribeMessage = 'Fail - This email is already in base...';
+                })
+        };
+
+        $scope.delete = function (email) {
+
+
+            $http.put('http://localhost:3000/subscribers', {
+                'email': email
+            })
+                .success(function () {
+                    $scope.subscribeMessage = 'Success - You remove your email to newsletter list';
+                })
+                .error(function () {
+                    $scope.subscribeMessage = 'Fail - Something went wrong...';
+                })
+        }
     });
